7702e43e2f392f799cb6776c0b81023841f7aac6
fallwlite <me@fallwlite.com>
2022-09-30 10:27:33 -0300

    Add colt info and improve script handler

--- react/Colt.jsx
+++ react/Colt.jsx
@@ -4,6 +4,10 @@ import { injectTrustVoxScripts } from "./services/ScriptHandler";
 import styles from './styles.css';
 
 const Colt = () => {
+  useEffect(() => {
+    injectTrustVoxScripts();
+  }, [])
+
   return (
     <div id="_trustvox_colt" className={styles.coltTrustvoxContainer} />
   )
