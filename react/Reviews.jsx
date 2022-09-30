f67b7f4a8547dc84034e1d5e0ef78d1fb648083a
fallwlite <me@fallwlite.com>
2022-09-30 09:38:43 -0300

    Fix product reviews info on script loading

--- react/Reviews.jsx
+++ react/Reviews.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { ProductContext } from 'vtex.product-context'
+import { injectTrustVoxScripts, setProduct } from "./services/ScriptHandler";
 
 import styles from './styles.css'
 
 const Reviews = () => {
+  const { product } = useContext(ProductContext)
+
+  if (!product) {
+    return null
+  }
+
+  const { productName, productId, items, productReference } = product
+
+  useEffect(() => {
+    setProduct({
+      productId,
+      productName,
+      imageUrl: items[0].images[0].imageUrl,
+      productReference,
+      sellers: null,
+      gtin: null,
+      ean: null,
+      department_id: null
+    });
+
+    injectTrustVoxScripts();
+  }, [product.productId]);
+
   return (
-    <div id="trustvox-reviews" className={styles.trustvoxContainer}>
-      <div id="_trustvox_widget" />
-    </div>
+    <>
+      <div id="trustvox-reviews" className={styles.trustvoxContainer}>
+        <div id="_trustvox_widget" />
+      </div>
+    </>
   )
 }
 
